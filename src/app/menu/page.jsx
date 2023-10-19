d7780cb93478ae17e59039c1b1c308cc9801ab3d
Nahid Hasan <nahidhasan1001@gmail.com>
2023-10-19 23:26:19 +0600

    menu 3

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -14,28 +14,28 @@ const Menu = async () => {
   const menu = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 flex items-center flex-col md:flex-row h-[calc(100vh-5rem)]">
-      {menu &&
-        menu.length &&
-        menu.map((cat) => (
-          <Link
-            key={cat.id}
-            href={`/menu/${cat.slug}`}
-            className="w-full p-8 h-1/3 md:h-[60%] bg-cover"
-            style={{ backgroundImage: `url(${cat.img})` }}
-          >
-            <div className={`text-${cat.color} w-1/2`}>
-              <h1 className="uppercase font-bold text-3xl">{cat.title}</h1>
-              <p className="text-sm my-4">{cat.desc}</p>
-              <button
-                className={` hidden xl:block bg-${cat.color} text-${
-                  cat.color === "black" ? "white" : "red-500"
-                } py-1 px-4 rounded-md `}
-              >
-                Explore
-              </button>
-            </div>
-          </Link>
-        ))}
+      {!menu && !menu.length
+        ? "nothing found in menu"
+        : menu.map((cat) => (
+            <Link
+              key={cat.id}
+              href={`/menu/${cat.slug}`}
+              className="w-full p-8 h-1/3 md:h-[60%] bg-cover"
+              style={{ backgroundImage: `url(${cat.img})` }}
+            >
+              <div className={`text-${cat.color} w-1/2`}>
+                <h1 className="uppercase font-bold text-3xl">{cat.title}</h1>
+                <p className="text-sm my-4">{cat.desc}</p>
+                <button
+                  className={` hidden xl:block bg-${cat.color} text-${
+                    cat.color === "black" ? "white" : "red-500"
+                  } py-1 px-4 rounded-md `}
+                >
+                  Explore
+                </button>
+              </div>
+            </Link>
+          ))}
     </div>
   );
 };
